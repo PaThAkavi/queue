26af4e9deacc7d9a9434f25d1011087832dd940b
Avaneesh Pathak <pathakavaneesh@gmail.com>
2019-10-05 22:30:10 +0530

    Added new file for static implementation of queue using arrays

---
+++ staticImpln.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+# define CAPACITY 5 
+void insert();
+void display();
+
+int queue[CAPACITY];
+int rear = 0;
+int front = 0;
+
+int main(){
+    insert();
+    insert();
+    insert();
+    display();
+    return 0;
+}
+
+void insert(){
+    int ele;
+    if(rear == CAPACITY - 1){
+        printf("Queue is full\n");
+    }
+    else{
+        printf("Enter the element to be inserted : \n");
+        scanf("%d", &ele);
+        rear+=1;
+        queue[rear] = ele;
+    }
+}
+
+void display(){
+    if(front == 0 && rear == 0){
+        printf("Queue is empty\n");
+    }
+    else{
+        while(rear != CAPACITY-1){
+            printf("%d, ", queue[rear]);
+            rear += 1;
+        }
+        printf("\n");
+    }
+}
